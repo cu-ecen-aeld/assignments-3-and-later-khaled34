58df76d9ddc1384e7fa3e13c3c11ba618cce5cde
khaled34 <engkhaledahmed34@gmail.com>
2025-01-18 17:20:08 +0200

    Assignment3 part1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 #include "systemcalls.h"
 
 /**
@@ -9,17 +13,161 @@
 */
 bool do_system(const char *cmd)
 {
+    int ret; 
+    bool result = false;
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+    /* Calling the system() system call */
+    ret = system(cmd);
+    /* If a child process could not be created, or its status could not be retrieved, the return value is -1 
+       and errno is set to indicate the  error*/
+    if (ret == -1)
+    {
+        perror("Child process could not be created");
+    }
+    else
+    {
+        /*
+            If  a  shell  could not be executed in the child process, then the return value is as though 
+            the child shell terminated by calling _exit(2) with the status 127.
+         */
+        if (ret == 127)
+        {
+            perror("Can't execute the Shell");
+        }
+        /* 
+            1. If  a  shell  could not be executed in the child process, then the return value is as though 
+            the child shell terminated by calling _exit(2) with the status 127.
 
-    return true;
+            2. If all system calls succeed, then the return value is the termination status of the child shell used to execute command.  
+            (The  termination status of a shell is the termination status of the last command it executes.)
+        */
+        if (WIFEXITED(ret)) 
+        {
+            printf("Child process exited with status: %d\n", WEXITSTATUS(ret));
+            result = true;
+        }
+        else
+        {
+            perror("Child process didn't exit the command normally\n");
+        }
+    }
+    printf("Final Status is %d\n", result);
+    return result;
 }
+/**
+ * @param command the parsed list of argument
+ * @param fd the file descriptor if exec to be redirected 
+ * @return boolean true in case of successful execution / false otherwise
+*/
+static bool common_do_exec(char * command[], int fd)
+{
+    bool result = false;
+    /* fork locals */
+    pid_t fork_pid;
+    /* execv locals */
+    /* wait locals */
+    pid_t wait_pid;
+    int status;
+    int saved_stderr, saved_stdout;
+    
 
+    if (fd > 2)
+    {
+        /* Save the stdout and stderr file descriptor to make it easy to return back */
+        saved_stdout = dup(1);
+        saved_stderr = dup(2);
+        if ((saved_stdout == -1) ||
+            (saved_stderr == -1)) 
+        {
+            perror("dup: failed to store stdout or stderr");
+            goto func_exit;
+        }
+        /* redirect the stdout and std error*/
+        if (dup2(fd, 1) < 0) 
+        { 
+            perror("dup2: Fatal Error STDOUT"); 
+            goto func_exit;
+        }
+        if (dup2(fd, 2) < 0) 
+        {
+            perror("dup2: Fatal Error STDERR"); 
+            goto func_exit;
+        }
+    }
+
+    /* Forking the child process */
+    fork_pid = fork();
+    if (fork_pid == -1)
+    {
+        perror("Fork: Error in forking the child");
+        goto func_exit;
+    }
+    /* Successfully forked */
+    if (fork_pid == 0)
+    {
+        /* Child code */
+        if(-1 == execv(command[0],&command[0]))
+        {
+            perror("Execv: Error in execv");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            exit(EXIT_SUCCESS);
+        }
+    }
+    else
+    {
+        /* Parent code */
+        wait_pid = waitpid (fork_pid, &status, 0);
+        if (wait_pid == -1)
+        {
+            perror ("wait");
+            goto func_exit;
+        }
+                
+        if (WIFEXITED (status))
+        {
+            printf ("Normal termination with exit status=%d\n",WEXITSTATUS (status));
+            if (WEXITSTATUS (status) == 0)
+            {
+                printf("SUCCESSFUL Status from Child\n");
+                result = true;
+            }
+            else
+            {
+                printf("Failure Status from Child\n");
+            }
+        }
+        else if (WIFSIGNALED (status))
+        {
+            /* Not necessary but added here for future use  */
+            printf ("Killed by signal=%d%s\n",WTERMSIG (status), WCOREDUMP (status) ? " (dumped core)" : "");
+        }
+        else
+        {
+            perror("Error Waiting Status");
+            goto func_exit;
+        }
+    }
+
+func_exit:
+    /* retore the direction of stdout/stderr */
+    if (fd > 2)
+    {
+        if (dup2(saved_stdout, 1) < 0) 
+        { 
+            perror("dup2: Fatal Error STDOUT"); 
+            goto func_exit;
+        }
+        if (dup2(saved_stderr, 2) < 0) 
+        {
+            perror("dup2: Fatal Error STDERR"); 
+            goto func_exit;
+        }
+    }
+    return result;
+}
 /**
 * @param count -The numbers of variables passed to the function. The variables are command to execute.
 *   followed by arguments to pass to the command
@@ -40,6 +188,8 @@ bool do_exec(int count, ...)
     va_start(args, count);
     char * command[count+1];
     int i;
+    bool result = false;
+
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
@@ -48,20 +198,12 @@ bool do_exec(int count, ...)
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
     command[count] = command[count];
-
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    
+    result  = common_do_exec(command, 0);
 
     va_end(args);
-
-    return true;
+    printf("Final Status is %d\n", result);
+    return result;
 }
 
 /**
@@ -71,10 +213,12 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+    bool result = false;
     va_list args;
     va_start(args, count);
     char * command[count+1];
     int i;
+
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
@@ -83,17 +227,45 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
     command[count] = command[count];
+    
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, S_IRUSR| S_IWUSR | S_IRGRP| S_IROTH);
+    if (fd == -1) 
+    { 
+        perror("open"); 
+        goto func_exit;
+    }    
+    /* call the main functionality */
+    result = common_do_exec(command, fd);
+    
+    if (-1 == close(fd))
+    {
+        perror("close: failed to close the file\n");
+        goto func_exit;
+    }
 
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-
+func_exit:
     va_end(args);
-
-    return true;
+    printf("Final Status is %d\n", result);
+    return result;
 }
+
+#if 0
+/* local driver */
+int main()
+{
+    bool result;
+    char* test = "ls";
+    char * args[] = { "ls", "-l", NULL };
+    // result = do_system(test);
+    // if (result == false){printf("failure in do_system\n"); return -1;}
+    // else {printf("SUCCESS in do_system\n");}
+    result = do_exec(3, "/usr/bin/test","-f","echo");//do_exec(3, test, args[0], args[1], args[2]);
+    if (result == false){printf("failure in do_exec\n");return -1;}
+    else {printf("SUCCESS in do_exec\n");}
+    // result = do_exec_redirect("/home/khaled/Desktop/file_test",3, test, args[0], args[1], args[2]);
+    // if (result == false){printf("failure in do_exec_redirect\n");return -1;}
+    // else {printf("SUCCESS in do_exec_redirect\n");}
+    printf("PASSED\n");
+    return 0;
+}
+#endif /* DEBUGGING */
